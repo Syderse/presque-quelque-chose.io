aea36bbc3de4c46a0020ed01870102616e9574bb
Syderse <mathieu.allag@gmail.com>
2025-11-15 15:12:30 +0000

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,9 @@ const { withHugoBlox } = require('./_vendor/github.com/hugoblox/hugo-blox-builde
 // 2. on utilise le plugin "withHugoBlox" pour envelopper la configuration.
 //    c'est lui qui va lire ton `params.yaml` (pour les polices)
 //    et ton `assets/css/themes/custom.css` (pour les couleurs).
+//    C'EST CETTE LIGNE QUI CORRIGE LE BUG ACTUEL (le nil pointer)
 module.exports = withHugoBlox({
-  
+
   // 3. on indique juste à tailwind quels *autres* fichiers surveiller.
   //    (le plugin s'occupe déjà de surveiller les fichiers du thème)
   content: [
